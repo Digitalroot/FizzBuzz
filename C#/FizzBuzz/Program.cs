570339e28f8ebcf67db202e2bf57e8c95ae15395
Digitalroot <digitalroot@gmail.com>
2013-06-24 20:13:20 -0700

    Added C# Test

---
+++ C#/FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace FizzBuzz
+{
+  class Program
+  {
+    static void Main()
+    {
+      for (int i = 1; i <= 100; i++)
+      {
+        Console.WriteLine(FizzBuzzCheck(i));
+      }
+      Console.ReadLine();
+    }
+
+    private static string FizzBuzzCheck(int i)
+    {
+      string output = String.Empty;
+
+      if (i % 3 == 0)
+      {
+        output = "Fizz";
+      }
+
+      if (i % 5 == 0)
+      {
+        output += "Buzz";
+      }
+
+      if (String.IsNullOrEmpty(output))
+      {
+        output = i.ToString(CultureInfo.InvariantCulture);
+      }
+
+      return output;
+    }
+  }
+}
